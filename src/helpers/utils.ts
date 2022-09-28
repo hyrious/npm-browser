cf668b493291bf5bf6426ddd17344b4f0c4e19ae
hyrious <hyrious@outlook.com>
2022-09-28 17:23:47 +0800

    handle git:// urls

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -8,5 +8,8 @@ export function normalize_git_repo(repo: string) {
   if (repo.startsWith("https://github.com/")) {
     repo = repo.slice(19);
   }
+  if (repo.startsWith("git://github.com/")) {
+    repo = repo.slice(17);
+  }
   return repo;
 }
